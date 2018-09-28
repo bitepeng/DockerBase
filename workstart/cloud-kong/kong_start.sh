ce728bffc755ab3809350f020abc63bf766547d0
bitepeng <bitepeng@qq.com>
2018-09-28 14:40:31 +0800

    Add kong

--- workstart/cloud-kong/kong_start.sh
+++ workstart/cloud-kong/kong_start.sh
@@ -3,5 +3,5 @@
 # 启动kong服务
 
 docker run --rm -p 8083:8080 pgbi/kong-dashboard start \
-  --kong-url http://120.79.117.198:8001 \
+  --kong-url http://10.92.102.214:8001 \
   --basic-auth admin=admin888
